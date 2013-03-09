4c92afe3f07730f07105212f8fac777120177501
Debjyoti Majumder <debjyoti.majumder@gmail.com>
2013-03-09 16:23:59 +0530

    Added devise email settings in production config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ MatchFinder::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  #Devise authentication
+  config.action_mailer.default_url_options = { :host => 'http://protected-harbor-8849.herokuapp.com' }
 end
