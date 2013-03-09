c76e8d68bbf012959e59bb8fd13d594252052c16
Debjyoti Majumder <debjyoti.majumder@gmail.com>
2013-03-09 18:18:26 +0530

    Setting up smtp for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,13 @@ MatchFinder::Application.configure do
   
   #Devise authentication
   config.action_mailer.default_url_options = { :host => 'http://protected-harbor-8849.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method ||= :smtp
 end
