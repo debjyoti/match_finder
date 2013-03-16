83bbfd4ff62aca60d2354923a7147b0cfff436df
Debjyoti Majumder <debjyoti.majumder@gmail.com>
2013-03-16 18:39:53 +0530

    Using gettoknowyou@gmail.com instead of debjyo... Also replaced heroku url with www.gettoknowyou.in

--- app/mailers/ping_mailer.rb
+++ app/mailers/ping_mailer.rb
@@ -1,5 +1,5 @@
 class PingMailer < ActionMailer::Base
-  default from: "debjyoti.majumder.02@gmail.com"
+  default from: "gettoknowyu@gmail.com"
 
   def ping_email(pinged_email, pinger_name, pinger_message, pinger_email, pinger_profile_url)
     @pinger_name = pinger_name
